dce453f7d5351a6e326df4fe31441cc14c0ec478
souri <ssak@outlook.de>
2023-12-09 11:21:41 +0100

    -

--- src/main/java/com/example/jwt/core/generic/ExtendedRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jwt.core.generic;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExtendedRepository<T extends ExtendedEntity> extends JpaRepository<T, UUID> {
-
-}
--- src/main/java/com/example/jwt/domain/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.jwt.domain.user;
-
-import com.example.jwt.core.generic.ExtendedService;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService, ExtendedService<User> {
-
-  User register(User user);
-}
