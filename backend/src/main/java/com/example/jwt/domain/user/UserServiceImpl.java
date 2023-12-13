b9a561d670640a2af8783872f8c96cfe53ccc13f
souri <ssak@outlook.de>
2023-12-13 19:25:07 +0100

    Authorities in UserServiceImpl push

--- backend/src/main/java/com/example/jwt/domain/user/UserServiceImpl.java
+++ backend/src/main/java/com/example/jwt/domain/user/UserServiceImpl.java
@@ -12,7 +12,19 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ExtendedServiceImpl<User> implements UserService {
 
   private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  @Override
+  public User register(User user) {
+    user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
+    user.setRole(Role.CLIENT);
+
+    Set<Authority> authorities = new HashSet<>();
+    authorities.add(Authority.CAN_PLACE_ORDER);
+    authorities.add(Authority.CAN_RETRIEVE_PURCHASE_HISTORY);
+    authorities.add(Authority.CAN_RETRIEVE_PRODUCTS);
+    user.setAuthorities(authorities);
+
+    return save(user);
   @Autowired
   public UserServiceImpl(UserRepository repository, Logger logger,
       BCryptPasswordEncoder bCryptPasswordEncoder) {
