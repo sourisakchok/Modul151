e6acb9905cd02c62fdbebda833fd0c179a369bda
souri <ssak@outlook.de>
2023-12-19 20:49:28 +0100

    Endpoint für React zusatzaufgabe

--- frontend/src/components/Products.tsx
+++ frontend/src/components/Products.tsx
@@ -1,8 +1,23 @@
 //TODO: Implement a products page that is only accessible to authenticated users with the Authority "CAN_RETRIEVE_PRODUCTS"
+
+import axios from 'axios';
+import { Card } from 'react-bootstrap';
 const Products = () => {
     return (
-        <div>
-            <h1>Authenticated - Place products here</h1>
+
+        // Authenticated
+        <div className="product-container">
+            {/*{products.map((product, index) => (*/}
+            {/*    <Card key={index} style={{ width: '18rem' }}>*/}
+            {/*        <Card.Img variant="top" src={product.imageUrl} />*/}
+            {/*        <Card.Body>*/}
+            {/*            <Card.Title>{product.name}</Card.Title>*/}
+            {/*            <Card.Text>*/}
+            {/*                {product.description}*/}
+            {/*            </Card.Text>*/}
+            {/*        </Card.Body>*/}
+            {/*    </Card>*/}
+            {/*))}*/}
         </div>
     )
 }
