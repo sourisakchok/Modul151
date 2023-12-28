e2dbe43bc9be6de5c5c02c74e9a147f9191e7063
souri <ssak@outlook.de>
2023-12-28 22:27:38 +0100

    sonarcloud

---
+++ backend/build/generated/sources/annotationProcessor/java/main/com/example/jwt/domain/order/dto/OrderMapperImpl.java
@@ -0,0 +1,55 @@
+package com.example.jwt.domain.order.dto;
+
+import com.example.jwt.domain.order.Order;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-12-28T22:18:59+0100",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 17.0.9 (Eclipse Adoptium)"
+)
+@Component
+public class OrderMapperImpl implements OrderMapper {
+
+    @Override
+    public Order fromOrderDTO(OrderDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Order order = new Order();
+
+        order.setId( dto.getId() );
+        order.setUser( dto.getUser() );
+        order.setProduct( dto.getProduct() );
+        if ( dto.getOrderDate() != null ) {
+            order.setOrderDate( LocalDateTime.ofInstant( dto.getOrderDate().toInstant(), ZoneOffset.UTC ).toLocalDate() );
+        }
+        order.setQuantity( dto.getQuantity() );
+
+        return order;
+    }
+
+    @Override
+    public OrderDTO toOrderDTO(Order dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        OrderDTO orderDTO = new OrderDTO();
+
+        orderDTO.setId( dto.getId() );
+        orderDTO.setUser( dto.getUser() );
+        orderDTO.setProduct( dto.getProduct() );
+        if ( dto.getOrderDate() != null ) {
+            orderDTO.setOrderDate( Date.from( dto.getOrderDate().atStartOfDay( ZoneOffset.UTC ).toInstant() ) );
+        }
+        orderDTO.setQuantity( dto.getQuantity() );
+
+        return orderDTO;
+    }
+}
